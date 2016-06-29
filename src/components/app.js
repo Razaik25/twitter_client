900d06e7707c4b82bc4275f88af4abd42cf54b94
Razaik Boparai <razaikb@gmail.com>
2016-06-29 20:24:28 +0530

    installed twitter

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ import { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Hello World!</div>
+      <div>Welcome, log in using Twitter and Tweet Away!!!</div>
     );
   }
 }
